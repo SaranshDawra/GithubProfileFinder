86cb0b8c5c9c742eb1b43a3767f543c9bd648595
Saransh Dawra <50369485+SaranshDawra@users.noreply.github.com>
2020-03-13 18:39:04 +0530

    Fetch Repositories

--- github.js
+++ github.js
@@ -1,12 +1,20 @@
 class Github {
+    constructor(){
+        this.repo_count = 5;
+        this.repo_sort = 'created: asc';
+    }
     async getUser(user) {
         console.log(user);
         const profileResponse = await fetch(`https://api.github.com/users/${user}`);
 
+        const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repo_count}&sort=${this.repo_sort}`);
+        
         const profileData = await profileResponse.json();
-
+        const repoData = await repoResponse.json();
+        
         return {
-            profile: profileData
+            profile: profileData,
+            repo: repoData
         }
     }
 }
